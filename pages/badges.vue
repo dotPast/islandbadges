5a617af92a4eca658c85507de641e46879450b87
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-04 12:24:08 +0300

    add a minimal badges page

---
+++ pages/badges.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div v-for="data in json">{{data.name}}</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import json from '/assets/badges.json';
+</script>
+
+<style></style>
