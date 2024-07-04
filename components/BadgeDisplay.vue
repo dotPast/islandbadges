b01419acf3f108b76839b8d8828293c9fada801d
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-04 13:13:31 +0300

    add a component for displaying badges

---
+++ components/BadgeDisplay.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{ name }} - {{ description }} - {{ solution }} - {{ icon }}
+  </div>
+</template>
+
+<script>
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      default: () => "Missing Badge",
+    },
+    icon: {
+      type: String,
+      default: () => "/assets/badges/missing.png",
+    },
+    description: {
+      type: String,
+      default: () => "?",
+    },
+    solution: {
+      type: String,
+      default: () => "?",
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
