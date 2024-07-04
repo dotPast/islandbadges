494929624746cd369e7b5c616ffbd4bb9d95f2e4
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-04 21:16:44 +0300

    add a page for all badges

--- pages/badges.vue
+++ pages/badges.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <div v-for="data in json"><BadgeDisplay :name="data.name" :icon="data.icon" :description="data.description" :solution="data.solution"/></div>
+    <div v-for="data in badges">
+      <BadgeDisplay :name="data.label" :icon="data.badgeIcon" :description="data.description" :solution="data.solution" />
+    </div>
+    
   </div>
 </template>
 
 <script lang="ts" setup>
-import json from '../public/assets/badges.json';
+import badges from '../public/assets/badges.json';
 </script>
 
 <style></style>
