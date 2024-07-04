3d6c17d6a6c1ad419c0557d30d7232a06bd08dc3
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-04 13:14:54 +0300

    use a badgedisplay component for listing all badges

--- pages/badges.vue
+++ pages/badges.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div v-for="data in json">{{data.name}}</div>
+    <div v-for="data in json"><BadgeDisplay :name="data.name" :icon="data.icon" :description="data.description" :solution="data.solution"/></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import json from '/assets/badges.json';
+import json from '../public/assets/badges.json';
 </script>
 
 <style></style>
