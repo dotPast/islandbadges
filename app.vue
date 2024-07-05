dcbcb5e767721d32251d382dc9425d23dac931c9
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-05 13:23:08 +0300

    implement badge categories

--- app.vue
+++ app.vue
@@ -22,7 +22,14 @@
 </template>
 
 <script setup lang="ts">
-import badges from './public/assets/badges.json';
+import bbBadges from './public/assets/list/bb.json';
+import dbBadges from './public/assets/list/db.json';
+import generalBadges from './public/assets/list/general.json';
+import hitwBadges from './public/assets/list/hitw.json';
+import pkwBadges from './public/assets/list/pkw.json';
+import rsrBadges from './public/assets/list/rsr.json';
+import sbBadges from './public/assets/list/sb.json';
+import tgttosBadges from './public/assets/list/tgttos.json';
 
 var searchOpen = ref(false)
 const commandPaletteRef = ref()
@@ -30,9 +37,45 @@ const commandPaletteRef = ref()
 const groups = computed(() =>
   [
     {
-      key: 'badges',
-      commands: badges
-    }
+      key: 'general',
+      label: 'General Badges',
+      commands: generalBadges
+    },
+    {
+      key: 'bb',
+      label: 'Battle Box Badges',
+      commands: bbBadges
+    },
+    {
+      key: 'db',
+      label: 'Dynaball Badges',
+      commands: dbBadges
+    },
+    {
+      key: 'hitw',
+      label: 'Hole in the Wall Badges',
+      commands: hitwBadges
+    },
+    {
+      key: 'pkw',
+      label: 'Parkour Warrior Badges',
+      commands: pkwBadges
+    },
+    {
+      key: 'rsr',
+      label: 'Rocket Spleef Rush Badges',
+      commands: rsrBadges
+    },
+    {
+      key: 'sb',
+      label: 'Sky Battle Badges',
+      commands: sbBadges
+    },
+    {
+      key: 'tgttos',
+      label: 'To Get to the Other Side Badges',
+      commands: tgttosBadges
+    },
   ].filter(Boolean))
 
 function onSelect (option) {
--- pages/badges.vue
+++ pages/badges.vue
@@ -1,9 +1,47 @@
 <template>
-  <IconBadgeDisplay :name="data.label" :icon="data.badgeIcon" :link="data.link" v-for="data in badges"/>
+  <div>
+    <h1 class="text-4xl mb-4">General</h1>
+    <IconBadgeDisplay v-for="badge in generalBadges" :name="badge.label" :icon="badge.badgeIcon" :link="badge.link"/>
+  </div>
+  <div>
+    <h1 class="text-4xl mb-4">Battle Box</h1>
+    <IconBadgeDisplay v-for="badge in bbBadges" :name="badge.label" :icon="badge.badgeIcon" :link="badge.link"/>
+  </div>
+  <div>
+    <h1 class="text-4xl mb-4">Dynaball</h1>
+    <IconBadgeDisplay v-for="badge in dbBadges" :name="badge.label" :icon="badge.badgeIcon" :link="badge.link"/>
+  </div>
+  <div>
+    <h1 class="text-4xl mb-4">Hole in the Wall</h1>
+    <IconBadgeDisplay v-for="badge in hitwBadges" :name="badge.label" :icon="badge.badgeIcon" :link="badge.link"/>
+  </div>
+  <div>
+    <h1 class="text-4xl mb-4">Parkour Warrior</h1>
+    <IconBadgeDisplay v-for="badge in pkwBadges" :name="badge.label" :icon="badge.badgeIcon" :link="badge.link"/>
+  </div>
+  <div>
+    <h1 class="text-4xl mb-4">Rocket Spleef Rush</h1>
+    <IconBadgeDisplay v-for="badge in rsrBadges" :name="badge.label" :icon="badge.badgeIcon" :link="badge.link"/>
+  </div>
+  <div>
+    <h1 class="text-4xl mb-4">Sky Battle</h1>
+    <IconBadgeDisplay v-for="badge in sbBadges" :name="badge.label" :icon="badge.badgeIcon" :link="badge.link"/>
+  </div>
+  <div>
+    <h1 class="text-4xl mb-4">To Get to the Other Side</h1>
+    <IconBadgeDisplay v-for="badge in tgttosBadges" :name="badge.label" :icon="badge.badgeIcon" :link="badge.link"/>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import badges from '../public/assets/badges.json';
+import bbBadges from '../public/assets/list/bb.json';
+import dbBadges from '../public/assets/list/db.json';
+import generalBadges from '../public/assets/list/general.json';
+import hitwBadges from '../public/assets/list/hitw.json';
+import pkwBadges from '../public/assets/list/pkw.json';
+import rsrBadges from '../public/assets/list/rsr.json';
+import sbBadges from '../public/assets/list/sb.json';
+import tgttosBadges from '../public/assets/list/tgttos.json';
 </script>
 
 <style></style>
