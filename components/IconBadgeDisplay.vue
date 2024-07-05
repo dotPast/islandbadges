e326d4e0c713ae893280f6cf5135d24b03ba71b2
dotPast <83116565+dotPast@users.noreply.github.com>
2024-07-05 12:24:56 +0300

    rename BadgeDisplay component to IconBadgeDisplay

---
+++ components/IconBadgeDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <a :href="link" class="mr-2">
+    <UTooltip :text="name">
+      <img :src="icon" width="96">
+    </UTooltip>
+  </a>
+</template>
+
+<script>
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      default: () => "The Badge is missing a name. Open an issue on the GitHub repository of IslandBadges.",
+    },
+    icon: {
+      type: String,
+      default: () => "/assets/badges/missing.png",
+    },
+    link: {
+      type: String,
+      default: () => "/badge/missing"
+    }
+  }
+})
+</script>
+
+<style></style>
